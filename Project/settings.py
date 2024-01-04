ff87bb1782e4980a2c55bfdc1f4cfd0b1fe79ed8
Son-of-Grace-001 <horlharmighty2000@gmail.com>
2024-01-04 11:32:35 -0800

    modified the admin.py

--- Project/settings.py
+++ Project/settings.py
@@ -37,10 +37,10 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
