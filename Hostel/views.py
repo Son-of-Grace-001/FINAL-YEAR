d17cd75cc0753ff19c037e3abbf75c59e89a9378
Son-of-Grace-001 <horlharmighty2000@gmail.com>
2024-01-05 18:01:44 -0800

    rectifying qr code issue

--- Hostel/views.py
+++ Hostel/views.py
@@ -22,6 +22,25 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib import colors
 
 def home (request):
+  if request == 'POST':
+    fname = request.POST.get('fname')
+    lname = request.POST.get('lname')
+    email = request.POST.get('email')
+    message = request.POST.get('message')
+
+    subject = "Thanks for contacting me"
+    body = f"Hello {fname} {lname},\n\nThank you for reaching out to me, your message has been received successfully, we will get back to you in due time\n\nWarm Regards,\n\nOlajire Stephen\n"
+
+    mail = EmailMessage(subject= subject, body=body, from_email=settings.EMAIL_HOST_USER , to = [email])
+    mail.send()
+    
+    subject = "New message Alert"
+    body = f"A new message was received from {fname} {lname}, with the message of '{message}', and a mail has been automatically sent to their eamil, which is {email} Please attend to it"
+    
+    mail = EmailMessage(subject= subject, body=body, from_email=settings.EMAIL_HOST_USER , to = [settings.EMAIL_HOST_USER])
+    mail.send()
+    messages.info(request, "Your message was sent successfully")
+      
   return render (request, 'hostel/home.html')
 
 @login_required
