8d1b43c94d5928da16128394f874756dde8ed212
Son-of-Grace-001 <horlharmighty2000@gmail.com>
2024-02-19 16:56:23 -0800

    supervisor

--- Project/settings.py
+++ Project/settings.py
@@ -12,11 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
 import dj_database_url
 from decouple import config
 # Load environment variables from .env file
-load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -92,23 +90,23 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / "db.sqlite3",
-#         'OPTIONS': {
-#             'timeout': 30,  # Adjust the timeout value as needed
-#         },
-#     },
-# }
-
 DATABASES = {
-    "default": dj_database_url.parse(config("external_url")),
-    # 'OPTIONS': {
-    #         'sslmode': 'require',  # or 'verify-full' for stricter verification
-    #         'sslrootcert': '/path/to/your/certificate.crt',
-    #     }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+        'OPTIONS': {
+            'timeout': 30,  # Adjust the timeout value as needed
+        },
+    },
 }
+
+# DATABASES = {
+#     "default": dj_database_url.parse(config("external_url")),
+#     # 'OPTIONS': {
+#     #         'sslmode': 'require',  # or 'verify-full' for stricter verification
+#     #         'sslrootcert': '/path/to/your/certificate.crt',
+#     #     }
+# }
 
 
 
